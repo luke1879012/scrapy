ebfd6d800f5d816b8c0cf1da033176ec5058dd76
luke <luke781520097@163.com>
2021-05-25 13:19:22 +0800

    scrapy工具增加注释

--- scrapy/utils/httpobj.py
+++ scrapy/utils/httpobj.py
@@ -6,14 +6,16 @@ from weakref import WeakKeyDictionary
 
 from scrapy.http import Request, Response
 
-
+# 弱引用字典做个缓存
 _urlparse_cache: "WeakKeyDictionary[Union[Request, Response], ParseResult]" = WeakKeyDictionary()
 
 
 def urlparse_cached(request_or_response: Union[Request, Response]) -> ParseResult:
     """Return urlparse.urlparse caching the result, where the argument can be a
     Request or Response object
+    返回urlparse.urlparse将结果缓存，其中参数可以是Request或Response对象
     """
     if request_or_response not in _urlparse_cache:
+        # 如果不在这个字典中，则生成ParseResult对象
         _urlparse_cache[request_or_response] = urlparse(request_or_response.url)
     return _urlparse_cache[request_or_response]
